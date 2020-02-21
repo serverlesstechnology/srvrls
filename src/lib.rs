664df11c510fe1f9f8bd9de2e16357eca85f9832
Dave Garred <dgarred@starbucks.com>
2020-02-21 06:47:25 -0800

    move functions to processors to add RPC support

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,102 @@
 use std::collections::HashMap;
+use std::fmt::Debug;
 
 use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
 use lambda_runtime::Context;
 use lambda_runtime::error::HandlerError;
-use crate::response::Response;
 
+use crate::response::Response;
 
 mod response;
 
+trait Processor {
+    fn process(&self, req: ApiGatewayProxyRequest) -> ApiGatewayProxyResponse;
+}
 pub type ProxyHandler = fn(ApiGatewayProxyRequest) -> ApiGatewayProxyResponse;
+pub type RpcHandler<T> = fn(T) -> ApiGatewayProxyResponse;
 
-pub struct App {
-    handlers: HashMap<String,ProxyHandler>,
+pub struct LambdaRouter {
+    handlers: HashMap<String, Box<dyn Processor>>,
 }
 
-impl App {
-    pub fn new()->Self {
-        App{
+impl LambdaRouter {
+    pub fn new() -> Self {
+        LambdaRouter {
             handlers: HashMap::new(),
         }
     }
-    pub fn register(&mut self, route: &str, f: ProxyHandler){
-        self.handlers.insert(route.to_string(), f);
+    pub fn register(&mut self, route: &str, f: ProxyHandler) {
+        self.handlers.insert(route.to_string(), Box::new(GenericProcessor::new(f)));
+    }
+    pub fn register_rpc<T: 'static + Default + Debug>(&mut self, route: &str, rpc_f: RpcHandler<T>) {
+        self.handlers.insert(route.to_string(), Box::new(RpcProcessor::new(rpc_f)));
     }
 }
 
-impl lambda_runtime::Handler<ApiGatewayProxyRequest, ApiGatewayProxyResponse, HandlerError> for App {
+struct GenericProcessor {
+    proxy_handler: ProxyHandler,
+}
+impl GenericProcessor {
+    fn new(proxy_handler: ProxyHandler)->Self {
+        GenericProcessor{
+            proxy_handler
+        }
+    }
+}
+impl Processor for GenericProcessor {
+    fn process(&self, req: ApiGatewayProxyRequest) -> ApiGatewayProxyResponse {
+        (self.proxy_handler)(req)
+    }
+}
+
+
+
+struct RpcProcessor<T: Default + Debug> {
+    h: RpcHandler<T>,
+}
+impl <T: Default + Debug> RpcProcessor<T> {
+    fn new(h: RpcHandler<T>) -> Self {
+        RpcProcessor {
+            h: h,
+        }
+    }
+    fn clean_val(&self)->T{
+        let val = T::default();
+        val
+    }
+}
+impl <T: Default + Debug> Processor for RpcProcessor<T> {
+    fn process(&self, req: ApiGatewayProxyRequest) -> ApiGatewayProxyResponse {
+        let dto = self.clean_val();
+        print!("{:?}", dto);
+        (self.h)(dto)
+    }
+}
+
+impl lambda_runtime::Handler<ApiGatewayProxyRequest, ApiGatewayProxyResponse, HandlerError> for LambdaRouter {
     fn run(&mut self, req: ApiGatewayProxyRequest, _ctx: Context) -> Result<ApiGatewayProxyResponse, HandlerError> {
         let path_url = &req.path_parameters["proxy"];
-        let path_iter : Vec<&str> = path_url.split("/").collect();
+        let path_iter: Vec<&str> = path_url.split("/").collect();
         let route = path_iter.first().unwrap();
         let result = match self.handlers.get(&route.to_string()) {
             None => Response::not_found(),
-            Some(f) => f(req),
+            Some(f) => f.process(req),
         };
         Ok(result)
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    use lambda_runtime::Handler;
+mod lambda_router_tests {
     use std::fs::File;
     use std::io::Read;
     use std::path::Path;
 
+    use lambda_runtime::Handler;
 
-    fn load_file(filename: &str) ->Result<String,std::io::Error> {
+    use super::*;
+
+    fn load_file(filename: &str) -> Result<String, std::io::Error> {
         let path = Path::new(filename);
         let mut file = File::open(&path)?;
 
@@ -62,19 +110,41 @@ mod tests {
     fn test_func(req: ApiGatewayProxyRequest) -> ApiGatewayProxyResponse {
         Response::ok("{}".to_string())
     }
+
+    #[derive(Default, Debug)]
+    struct TestDto {
+        id: String,
+        name: String,
+    }
+
+    fn test_rpc_func(dto: TestDto) -> ApiGatewayProxyResponse {
+        Response::ok("{}".to_string())
+    }
+
     #[test]
-    fn it_works() {
-        let mut app = App::new();
+    fn test_register() {
+        let mut app = LambdaRouter::new();
         app.register("target", test_func);
 
         let s = load_file("test_data/happy_path_event.json").unwrap();
-        let event : ApiGatewayProxyRequest = serde_json::from_str(s.as_str()).unwrap();
+        let event: ApiGatewayProxyRequest = serde_json::from_str(s.as_str()).unwrap();
         let response = app.run(event, Context::default()).unwrap();
         assert_eq!(Response::ok("{}".to_string()), response);
 
         let s = load_file("test_data/not_supported_event.json").unwrap();
-        let event : ApiGatewayProxyRequest = serde_json::from_str(s.as_str()).unwrap();
+        let event: ApiGatewayProxyRequest = serde_json::from_str(s.as_str()).unwrap();
         let response = app.run(event, Context::default()).unwrap();
         assert_eq!(Response::not_found(), response);
+    }
+
+    #[test]
+    fn test_rpc_register() {
+        let mut app = LambdaRouter::new();
+        app.register_rpc("target", test_rpc_func);
+
+        let s = load_file("test_data/happy_path_event.json").unwrap();
+        let event: ApiGatewayProxyRequest = serde_json::from_str(s.as_str()).unwrap();
+        let response = app.run(event, Context::default()).unwrap();
+        assert_eq!(Response::ok("{}".to_string()), response);
     }
 }
